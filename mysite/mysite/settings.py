2409ee369df9d57c1814b5e21bfb284e8bcb0bdd
Nima Shafie <nimzshafie@gmail.com>
2023-01-10 23:39:29 -0800

    hiding database cred, configuring postgreSQL

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -72,11 +72,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+# old sqlite defualt
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# using postgresql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'my_service',
+            'passfile': '.my_pgpass',
+        },
     }
 }
 
